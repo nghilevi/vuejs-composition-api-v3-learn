e5e20a094a597892e048efc121893a53d7d482e1
Nghi Le <vinhnghi223@gmail.com>
2023-11-27 21:20:39 +0200

    sec3 watch vs watchEffect

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 // imports
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, watch, watchEffect } from 'vue'
 import { TimelinePost } from '../posts'
 import { marked } from 'marked'
 
@@ -11,10 +11,36 @@ const props = defineProps<{ post: TimelinePost }>()
 const title = ref(props.post.title)
 const content = ref(props.post.markdown)
 const contentEditable = ref<HTMLDivElement>()
-const html = ref('')
+const html = ref('') // do not considered as reactive changes
 
+/*
+// only executed once
 marked.parse(content.value, (err, parseResult) => {
     html.value = parseResult
+})
+*/
+/*
+// more concise
+watchEffect(() => {
+    // Every time a reactive variable inside the callback is changed, it's going to execute the cb
+    marked.parse(content.value,( err, parseResult) => {
+        html.value = parseResult
+    })
+})
+*/
+
+// watch is only called when variable changes
+// more explicit
+watch(content, (newContent) => {
+    marked.parse(newContent, (_err: any, parseResult: string) => {
+        html.value = parseResult
+    })
+}, {
+    /*
+    . called the very first time the comp is created
+    . called every time content changes
+    */
+    immediate: true
 })
 
 // lifecycle hooks
@@ -40,8 +66,6 @@ function handleInput(){
             <div class="field">
                 <div class="label">Post title</div>
                 <input type="text" class="input" v-model="title">
-                <!-- <input type="text" class="input" :value="title" @input="event => title.value = event.target.value"> -->
-                {{ title }}
             </div>
         </div>
     </div>
