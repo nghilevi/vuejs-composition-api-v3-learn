3333f8f4c61b164de60a426b610b061d9c2bcddc
Nghi Le <vinhnghi223@gmail.com>
2023-11-28 04:22:01 +0200

    sec9 created Node POST endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,15 +2,22 @@
 
 import cors from "cors";
 import express from "express";
-import { thisMonth, thisWeek, today } from "../posts";
+import { Post, thisMonth, thisWeek, today } from "../posts";
+import bodyParser from "body-parser";
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
+const allPosts = [today, thisWeek, thisMonth]
 
 app.get('/posts', (_req, res) => {
-    res.json([
-        today, thisWeek, thisMonth
-    ])
+    res.json(allPosts)
+})
+
+app.post<{}, {}, Post>('/posts', (req, res) => {
+    const post = {...req.body, id: (Math.random()*100000).toFixed()}
+    allPosts.push(post)
+    res.json(post)
 })
 
 app.listen(8000, () => {
